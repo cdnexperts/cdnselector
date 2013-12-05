7d0c41e1e1e0c1e7e65a341afcfa027949a05d90
Tony Shearer <tonyshearer@gmail.com>
2013-12-05 11:50:03 -0300

    Added very simple HTTP Basic auth to the admin console

--- libs/console.js
+++ libs/console.js
@@ -20,6 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.basicAuth(localConfig.consoleUser, localConfig.consolePass));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, '../public')));
 
--- libs/localConfig.js
+++ libs/localConfig.js
@@ -10,6 +10,8 @@ module.exports = {
     dbUrl: process.env.CDNS_DB_URL || 'http://admin:cdnsadmin@localhost:5984',
     port: process.env.CDNS_PORT || 8888,
     consolePort: process.env.CDNS_CONSOLE_PORT || 3000,
+    consoleUser: process.env.CDNS_CONSOLE_USER || 'admin',
+    consolePass: process.env.CDNS_CONSOLE_PASS || 'admin',
     logLevel: process.env.CDNS_LOG_LEVEL || 'info',
     logDir: process.env.CDNS_LOG_DIR || 'log',
     logRotationInterval: process.env.CDNS_LOG_ROTATION_INTERVAL || 3600,
